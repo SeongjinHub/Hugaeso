c3f75488c358a3ed1e64ce44a8e41a6e25200d47
jeongjiwon <kzxn4269@gmail.com>
2023-05-17 19:48:20 +0900

    Google Maps API 추가
    임시로 Feed 탭에 동적으로 지도 프래그먼트를 추가하였습니다.
    
    2023-05-15

--- app/src/main/java/com/teamhgs/maptrips/FeedFragment.java
+++ app/src/main/java/com/teamhgs/maptrips/FeedFragment.java
@@ -10,12 +10,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link FeedFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class FeedFragment extends Fragment {
+public class FeedFragment extends Fragment implements OnMapReadyCallback{
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -55,11 +59,23 @@ public class FeedFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_feed, container, false);
+        ViewGroup viewGroup = (ViewGroup) inflater.inflate(R.layout.fragment_feed, container, false);
+
+        SupportMapFragment supportMapFragment = SupportMapFragment.newInstance();
+        getChildFragmentManager().beginTransaction()
+                .add(R.id.mapView2, supportMapFragment)
+                .commitAllowingStateLoss();
+
+        return viewGroup;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
     }
 }
