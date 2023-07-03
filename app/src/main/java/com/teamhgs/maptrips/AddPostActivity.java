997b57ec33b331dd78c44857817b6e18dc2fed60
jeongjiwon <kzxn4269@gmail.com>
2023-07-03 16:36:38 +0900

    게시글 작성 중 폴더 관련 기능 구현 중.
    DB에 저장된 폴더 정보를 불러와 선택이 가능합니다.
    
    2023-07-03

--- app/src/main/java/com/teamhgs/maptrips/AddPostActivity.java
+++ app/src/main/java/com/teamhgs/maptrips/AddPostActivity.java
@@ -4,7 +4,9 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
@@ -21,13 +23,19 @@ import android.text.TextWatcher;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Display;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -58,6 +66,7 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -87,13 +96,17 @@ public class AddPostActivity extends AppCompatActivity {
     String date;
     Button calendarButton;
     Button placeButton;
+    Button folderButton;
     Calendar calendar = Calendar.getInstance();
     ExifInterface exifInterface;
     String latitude;
     String longitude;
     String placeID;
     String predictedPlaceID;
+    Folder folder = null;
+    boolean firstFolderBtnClicked = true;
     ArrayList<String> placeIDArrayList = new ArrayList<>();
+    ArrayList<Folder> folderArrayList = new ArrayList<>();
 
     private PlacesClient placesClient;
 
@@ -145,6 +158,7 @@ public class AddPostActivity extends AppCompatActivity {
         TextView calenderText = (TextView) findViewById(R.id.text_calendar);
         calendarButton = (Button) findViewById(R.id.button_calander);
         placeButton = (Button) findViewById(R.id.button_place);
+        folderButton = (Button) findViewById(R.id.button_folder);
 
         date = calendar.get(Calendar.YEAR) + "-" + (calendar.get(Calendar.MONTH) + 1) + "-" + calendar.get(Calendar.DATE);
         String temp = calendar.get(Calendar.YEAR) + getString(R.string.activity_add_post_date_year)
@@ -362,7 +376,7 @@ public class AddPostActivity extends AppCompatActivity {
 
         Intent intent = new Autocomplete.IntentBuilder(AutocompleteActivityMode.FULLSCREEN, placeFields)
                 .setTypesFilter(Arrays.asList(PlaceTypes.ESTABLISHMENT)) // 비즈니스 정보가 등록된 장소로 검색범위를 제한
-                .build(getApplicationContext());
+                .build(this);
 
         placeButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -372,6 +386,81 @@ public class AddPostActivity extends AppCompatActivity {
             }
         });
 
+        LayoutInflater layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View folderDialogView = layoutInflater.inflate(R.layout.activity_add_post_folder, null, false);
+        Dialog folderDialog = new Dialog(this, R.style.FullScreenDialogTheme);
+        folderDialog.setContentView(folderDialogView);
+
+        ListView listViewFolder = (ListView) folderDialogView.findViewById(R.id.listview_folder);
+        ListViewAdapter listViewAdapter = new ListViewAdapter();
+        listViewFolder.setAdapter(listViewAdapter);
+
+        listViewFolder.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                ListViewItem item = (ListViewItem) parent.getItemAtPosition(position);
+
+                folder = item.getFolder();
+                folderButton.setText(item.getFolder().getTitle());
+                folderButton.setTextColor(getColor(R.color.black));
+
+                folderDialog.dismiss();
+            }
+        });
+
+        Response.Listener<String> getFolderResponseListener = new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONArray jsonResponse = new JSONArray(response);
+                    JSONObject jsonObject;
+
+                    if (jsonResponse.length() > 0) {
+                        for (int i = 0; i < jsonResponse.length(); i++) {
+
+                            jsonObject = jsonResponse.getJSONObject(i);
+                            String foldercode = jsonObject.getString("foldercode");
+
+                            Folder tempFolder = new Folder(foldercode);
+
+                            tempFolder.setTitle(jsonObject.getString("title"));
+                            tempFolder.setText(jsonObject.getString("text"));
+                            tempFolder.setStartDate(jsonObject.getString("startdate"));
+                            tempFolder.setEndDate(jsonObject.getString("enddate"));
+                            tempFolder.setUpdateDate(jsonObject.getString("updatedate"));
+                            tempFolder.setPrivateStatus(jsonObject.getInt("private"));
+
+                            listViewAdapter.addItem(tempFolder);
+
+                            folderArrayList.add(tempFolder);
+                        }
+                        folderDialog.show();
+                    } else {
+                        folderDialog.show();
+                        // 생성한 폴더가 없음을 알리는 코드
+                    }
+                } catch (JSONException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        };
+
+        folderButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (firstFolderBtnClicked) {
+                    Folder.getFolderRequest request = new Folder.getFolderRequest(currentUser.getUsercode(), getFolderResponseListener);
+                    RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+                    queue.add(request);
+                    firstFolderBtnClicked = false;
+                }
+                else {
+                    folderDialog.show();
+                }
+            }
+        });
     }
 
     @Override
@@ -414,13 +503,23 @@ public class AddPostActivity extends AppCompatActivity {
 
                             date = exifDate.substring(0, 10); // 2023-05-15 22-13-34 to 2023-05-15
 
-                            calendar.set(Calendar.YEAR, Integer.parseInt(date.substring(0, 4)));
-                            calendar.set(Calendar.MONTH, Integer.parseInt(date.substring(5, 7)) - 1);
-                            calendar.set(Calendar.DATE, Integer.parseInt(date.substring(8, 10)));
+                            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
-                            String temp = calendar.get(Calendar.YEAR) + getString(R.string.activity_add_post_date_year)
-                                    + (calendar.get(Calendar.MONTH) + 1) + getString(R.string.activity_add_post_date_month)
-                                    + calendar.get(Calendar.DATE) + getString(R.string.activity_add_post_date_day);
+                            Date formattedDate = null;
+
+                            try {
+                                formattedDate = simpleDateFormat.parse(date);
+                            } catch (ParseException e) {
+                                throw new RuntimeException(e);
+                            }
+
+                            Calendar exifCalendar = Calendar.getInstance();
+
+                            exifCalendar.setTime(formattedDate);
+
+                            String temp = exifCalendar.get(Calendar.YEAR) + getString(R.string.activity_add_post_date_year)
+                                    + (exifCalendar.get(Calendar.MONTH) + 1) + getString(R.string.activity_add_post_date_month)
+                                    + exifCalendar.get(Calendar.DATE) + getString(R.string.activity_add_post_date_day);
                             calendarButton.setText(temp);
                             calendarButton.setTextColor(getColor(R.color.app_main_color));
 
@@ -681,5 +780,124 @@ public class AddPostActivity extends AppCompatActivity {
 
         return result;
 
+    }
+
+    // 리스트 뷰에 표시 할 객체
+    public class ListViewItem {
+        private ImageView imageView;
+        private Folder folder;
+
+        public ImageView getImageView() {
+            return imageView;
+        }
+
+        public void setImageView(String url) {
+//            Glide로 이미지 삽입하는 코드
+        }
+
+        public Folder getFolder() {
+            return folder;
+        }
+
+        public void setFolder(Folder folder) {
+            this.folder = folder;
+        }
+    }
+
+    // 리스트 뷰에 객체를 추가하여 화면에 출력
+    public class ListViewAdapter extends BaseAdapter {
+
+        private ArrayList<ListViewItem> listViewItemList = new ArrayList<ListViewItem>();
+
+        @Override
+        public int getCount() {
+            return listViewItemList.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return listViewItemList.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            final int pos = position;
+            final Context context = parent.getContext();
+
+            // activity_add_post_folder_listitem을 inflate하여 레이아웃을 View로 만들고 convertView 참조 획득
+            if (convertView == null) {
+                LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                convertView = inflater.inflate(R.layout.activity_add_post_folder_listitem, parent, false);
+            }
+
+            // 화면에 표시될 View 객체 불러오기
+            ImageView ImageView = (ImageView) convertView.findViewById(R.id.imageView_listitem);
+            TextView titleTextView = (TextView) convertView.findViewById(R.id.text_listitem_title);
+            TextView subTextView = (TextView) convertView.findViewById(R.id.text_listitem_sub);
+
+            // Data Set (listViewItemList) 에서 position에 위치한 데이터참조 획득
+            ListViewItem listViewItem = listViewItemList.get(position);
+
+            // 각 객체에 값 입력
+//            iconImageView 에 Glide() 로 사진 입력 코드
+            titleTextView.setText(listViewItem.getFolder().getTitle());
+
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+            // 폴더의 시작 날짜와 끝 날짜를 가져옵니다.
+            Date startDate = null;
+            Date endDate = null;
+
+            try {
+                startDate = simpleDateFormat.parse(listViewItem.getFolder().getStartDate());
+                endDate = simpleDateFormat.parse(listViewItem.getFolder().getEndDate());
+            } catch (ParseException e) {
+                throw new RuntimeException(e);
+            }
+
+            Calendar calendarStart = Calendar.getInstance();
+            Calendar calendarEnd = Calendar.getInstance();
+
+            calendarStart.setTime(startDate);
+            calendarEnd.setTime(endDate);
+
+            String startDateText = calendarStart.get(Calendar.YEAR) + getString(R.string.activity_add_post_date_year)
+                    + (calendarStart.get(Calendar.MONTH) + 1) + getString(R.string.activity_add_post_date_month)
+                    + calendarStart.get(Calendar.DATE) + getString(R.string.activity_add_post_date_day);
+
+            String endDateText = calendarEnd.get(Calendar.YEAR) + getString(R.string.activity_add_post_date_year)
+                    + (calendarEnd.get(Calendar.MONTH) + 1) + getString(R.string.activity_add_post_date_month)
+                    + calendarEnd.get(Calendar.DATE) + getString(R.string.activity_add_post_date_day);
+
+            // 가져온 결과를 화면에 출력합니다.
+            String textSub = startDateText + "  ~  " + endDateText + " ";
+
+            subTextView.setText(textSub);
+
+            return convertView;
+        }
+
+        // item에 데이터 추가 - 추후 이미지 표시 기능 사용할 때 사용
+        public void addItem(String url, Folder folder) {
+            ListViewItem item = new ListViewItem();
+
+            item.setImageView(url);
+            item.setFolder(folder);
+
+            listViewItemList.add(item);
+        }
+
+        public void addItem(Folder folder) {
+            ListViewItem item = new ListViewItem();
+
+            item.setFolder(folder);
+
+            listViewItemList.add(item);
+        }
     }
 }
